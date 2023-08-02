685e6c10b52d2b246d1d4073e217e0d1f386231c
tuan <ndtuan1482000@gmail.com>
2023-08-02 13:08:26 +0700

    new

--- mysite/mysite/miku/ggmap/static/ggmap/script.js
+++ mysite/mysite/miku/ggmap/static/ggmap/script.js
@@ -1,17 +1,33 @@
-function createMap(lat,lng){
-			  const map = new google.maps.Map(document.getElementById('map'), {
-				center: {lat: 20.32156, lng: 105.98611},
-				zoom: 14,
-			  });
-			  new google.maps.Marker({
-			  	position: {lat: lat , lng: lng},
-				map,
-			  });
-			  const infowindow = new google.maps.InfoWindow({
-				content: "<p>Marker Location:" + marker.getPosition() + "</p>",
-			  });
-			  google.maps.event.addListener(marker, "click", () => {
-				infowindow.open(map, marker);
-			  });
-			}
+var firebaseConfig = {
+  	apiKey: "AIzaSyDnyzhNNeEORTPNdmc8sfFxl13hZx_t73k",
+  	authDomain: "lidar-ea564.firebaseapp.com",
+  	databaseURL: "https://lidar-ea564-default-rtdb.firebaseio.com",
+  	projectId: "lidar-ea564",
+  	storageBucket: "lidar-ea564.appspot.com",
+  	messagingSenderId: "692475158447",
+  	appId: "1:692475158447:web:d0a3fdf6f785447b50995a",
+  	measurementId: "G-35VBR13BXS"
+ };
 
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  var Command = firebase.database().ref('Command');
+  var ForwardTime = firebase.database().ref('ForwardTime')
+
+function setData(Cmd){
+    var Command = firebase.database().ref('Command');
+	Command.set(Cmd)
+	.then(function () {
+        console.log("Dữ liệu đã được gửi thành công lên Firebase!");
+      })
+      .catch(function (error) {
+        console.error("Lỗi khi gửi dữ liệu lên Firebase: ", error);
+      });
+
+}
+function defaut(){
+		var cmd = "STOP"
+		setData(cmd);
+}
+defaut();
